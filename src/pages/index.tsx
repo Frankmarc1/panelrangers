c169979e43aec53ca59022887c05c2cc28909b1d
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-08-09 09:14:33 -0500

    llamar HOC dashboard

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+import { Dashboard } from '../layout/Dashboard/Dashboard';
+
+const Home: NextPage = () => {
+  return (
+    <Dashboard>
+      <h3>helloo</h3>
+      <button>next</button>
+    </Dashboard>
+  );
+};
+
+export default Home;
