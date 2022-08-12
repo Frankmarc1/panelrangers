3248f8316f6be25bb29fd7df2ce2be0d57ec2136
Termux-Master93 <brisantur8034@gmail.com>
2022-08-11 20:10:10 -0500

    se arreglo los tamaños de los iconos en estadisticas

--- src/pages/dashboard/estadisticas/index.tsx
+++ src/pages/dashboard/estadisticas/index.tsx
@@ -1,15 +1,13 @@
-import Image from "next/image";
 import { FaMotorcycle, FaGripVertical } from "react-icons/fa";
 import { MdHomeWork } from "react-icons/md";
 import { Dashboard } from "../../../layout/Dashboard/Dashboard";
-import { Navbar } from "../../../layout/Dashboard/components/Navbar";
 const Statistics = () => {
   return (
     <Dashboard>
       <section className='bg-slate-200 h-full'>
         <div className='container m-auto md:grid md:grid-cols-2 md:gap-4 xl:grid xl:grid-cols-3 xl:gap-4 mt-5'>
           <div className='card card-side bg-base-100 shadow-xl grid grid-cols-2 mb-4'>
-            <div className='w-full text-[8rem] bg-blue grid items-center place-content-center bg-cyan-500 text-white'>
+            <div className='w-full text-[6rem] bg-blue grid items-center place-content-center bg-cyan-500 text-white'>
               <FaMotorcycle />
             </div>
             <div className='card-body'>
@@ -19,7 +17,7 @@ const Statistics = () => {
           </div>
 
           <div className='card card-side bg-base-100 shadow-xl grid grid-cols-2 mb-4'>
-            <div className='w-full text-[8rem] bg-blue grid items-center place-content-center bg-green-500 text-white'>
+            <div className='w-full text-[6rem] bg-blue grid items-center place-content-center bg-green-500 text-white'>
               <FaGripVertical />
             </div>
             <div className='card-body'>
@@ -29,7 +27,7 @@ const Statistics = () => {
           </div>
 
           <div className='card card-side bg-base-100 shadow-xl grid grid-cols-2 mb-4'>
-            <div className='w-full text-[8rem] bg-blue grid items-center place-content-center bg-slate-500 text-white'>
+            <div className='w-full text-[6rem] bg-blue grid items-center place-content-center bg-slate-500 text-white'>
               <MdHomeWork />
             </div>
             <div className='card-body'>
