81a1ce9516f92278f7ffb839d74d95aebb0b9144
Termux-Master93 <brisantur8034@gmail.com>
2022-08-12 13:24:09 -0500

    se iso el diseño de tabla para el partado usuarios

---
+++ src/pages/dashboard/usuarios/index.tsx
@@ -0,0 +1,10 @@
+import { Dashboard } from "../../../layout/Dashboard/Dashboard";
+import DataTable from "../../../layout/Dashboard/components/DataTable";
+const Users = () => {
+  return (
+    <Dashboard>
+      <DataTable />
+    </Dashboard>
+  );
+};
+export default Users;
