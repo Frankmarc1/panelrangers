85f49adea734ec53c6964c075aff4be3a294d16f
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-08-15 09:39:32 -0500

    remplazar font awesome por react-icons

--- src/layout/Dashboard/components/Navbar.tsx
+++ src/layout/Dashboard/components/Navbar.tsx
@@ -1,23 +1,22 @@
-import { faBars, faList } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Link from "next/link";
+import Link from 'next/link';
+import { FaBars, FaList } from 'react-icons/fa';
+import { useMediaQuery } from 'react-responsive';
 
-import { useRouteMatch } from "../../../hooks/useRouteMatch";
-
-import { Sidebar } from "./Sidebar";
-import { useMediaQuery } from "react-responsive";
-import { useFavicon } from "../../../hooks/useFavicon";
+import { useRouteMatch } from '../../../hooks/useRouteMatch';
+import { Sidebar } from './Sidebar';
+import { useFavicon } from '../../../hooks/useFavicon';
 
 export const Navbar = () => {
   const searchPath = useRouteMatch();
-  const isMovil = useMediaQuery({ maxWidth: "596px" });
+  const isMovil = useMediaQuery({ maxWidth: '596px' });
   const icon = useFavicon();
+
   return (
     <nav className='navbar bg-base-100 w-full'>
       <div className='navbar-start pl-[1rem]'>
         <div className='dropdown'>
-          <label tabIndex={0} className='w-[1.2rem] btn btn-ghost btn-circle'>
-            <FontAwesomeIcon icon={faBars} />
+          <label tabIndex={0} className=' btn btn-ghost btn-circle'>
+            <FaBars />
           </label>
           <ul
             tabIndex={0}
@@ -27,34 +26,34 @@ export const Navbar = () => {
               <Link href='/dashboard/estadisticas'>
                 <a
                   className={
-                    searchPath("estadisticas") ? "text-blue-500 font-bold" : ""
+                    searchPath('estadisticas') ? 'text-blue-500 font-bold' : ''
                   }
                 >
-                  {" "}
-                  Estadisticas{" "}
+                  {' '}
+                  Estadisticas{' '}
                 </a>
               </Link>
-              <Link href={"/dashboard/usuarios"}>
+              <Link href={'/dashboard/usuarios'}>
                 <a
                   className={
-                    searchPath("usuarios") ? "text-blue-500 font-bold" : ""
+                    searchPath('usuarios') ? 'text-blue-500 font-bold' : ''
                   }
                 >
-                  {" "}
-                  Usuarios{" "}
+                  {' '}
+                  Usuarios{' '}
                 </a>
               </Link>
             </li>
 
             <li>
-              <Link href='/dashboard/repartidores'>
+              <Link href='/dashboard/motorizados'>
                 <a
                   className={
-                    searchPath("repartidores") ? "text-blue-500 font-bold" : ""
+                    searchPath('motorizados') ? 'text-blue-500 font-bold' : ''
                   }
                 >
-                  {" "}
-                  Repartidores{" "}
+                  {' '}
+                  Motorizados{' '}
                 </a>
               </Link>
             </li>
@@ -63,11 +62,11 @@ export const Navbar = () => {
               <Link href='/dashboard/agencias'>
                 <a
                   className={
-                    searchPath("agencias") ? "text-blue-500 font-bold" : ""
+                    searchPath('agencias') ? 'text-blue-500 font-bold' : ''
                   }
                 >
-                  {" "}
-                  Agencias{" "}
+                  {' '}
+                  Agencias{' '}
                 </a>
               </Link>
             </li>
@@ -75,7 +74,7 @@ export const Navbar = () => {
         </div>
       </div>
       <div className='navbar-center'>
-        <Link href={"/dashboard"}>
+        <Link href={'/dashboard'}>
           <a className='btn btn-ghost normal-case text-xl'>Rangers Dashboard</a>
         </Link>
       </div>
@@ -88,7 +87,7 @@ export const Navbar = () => {
         ) : (
           <div className='dropdown dropdown-end'>
             <label tabIndex={0} className='btn btn-ghost btn-circle'>
-              <FontAwesomeIcon icon={faList} />
+              <FaList />
             </label>
             <ul
               tabIndex={0}
