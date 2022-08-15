a5a14cca30248b20c025bd0b51cb602d89bcc3b4
Termux-Master93 <86426464+Termux-Master93@users.noreply.github.com>
2022-08-15 13:58:01 -0500

    se esta creando listado de usuarios

--- src/pages/dashboard/usuarios/index.tsx
+++ src/pages/dashboard/usuarios/index.tsx
@@ -1,10 +1,29 @@
 import { Dashboard } from '../../../layout/Dashboard/Dashboard';
-
-const Users = () => {
+import { collection } from 'firebase/firestore';
+import {RowUsers} from '../../../app/users/RowUsers';
+import {FirebaseDataTable} from '../../../components/FirebaseDataTable/FirebaseDataTable';
+import { db_client } from '../../../firebase/client';
+const colRef = collection(db_client, 'rangers_masters');
+console.log(colRef)
+const Userss = () => {
   return (
     <Dashboard>
-      <h2>Usuarios</h2>
+      <FirebaseDataTable
+        headers={[
+          'D.N.I',
+          'NOMBRE Y APELLIDOS',
+          'ROL',
+          'ACCIONES',
+        ]}
+        RowComponent={RowUsers}
+        
+        qi={colRef}
+      
+      />
+    
     </Dashboard>
+
   );
+  
 };
-export default Users;
+export default Userss;
