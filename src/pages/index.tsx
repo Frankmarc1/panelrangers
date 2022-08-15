57473422a5d726856da76de561320e36595e9500
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-08-15 09:39:49 -0500

    remplazar por hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,7 @@ import { Dashboard } from '../layout/Dashboard/Dashboard';
 const Home: NextPage = () => {
   return (
     <Dashboard>
-      <h3>helloo</h3>
-      <button>next</button>
+      <h1> Bienvenido </h1>
     </Dashboard>
   );
 };
