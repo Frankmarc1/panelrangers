4d907af2b6b42406ce29daf564b30e9e54205838
Termux-Master93 <86426464+Termux-Master93@users.noreply.github.com>
2022-08-17 14:05:10 -0500

    se creo el listado de agencias y el componente spiner

---
+++ src/app/agencies/RowAgencies.tsx
@@ -0,0 +1,19 @@
+import { Agencie } from '../../types/agencies';
+import { DateTime } from 'luxon';
+import { FaPen, FaUserAltSlash } from 'react-icons/fa';
+
+export const RowAgencies= ({ values }: { values: Agencie }) => {
+  return (
+    <tr>
+      <td> {values.nombre} </td>
+      <td> {values.departamento} </td>
+      <td>{values.direccion}</td>
+      <td> {values.estado ? 'Activo' : 'Inactivo'} </td>
+      <td> {' '}
+        {DateTime.fromSeconds(values.fecha_registro.seconds).toFormat(
+          'dd/MM/yyyy'
+        )}{' '}
+      </td>
+    </tr>
+  );
+};
