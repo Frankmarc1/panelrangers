43a77460bb55c508b3c88e4e2fc0aa8d531bde4f
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-08-19 10:44:44 -0500

    limpiar directorio

--- src/types/agency.ts
+++
@@ -1,9 +0,0 @@
-import { Timestamp } from 'firebase/firestore';
-
-export interface Agency {
-  nombre: string;
-  departamento: string;
-  direccion: string;
-  estado: boolean;
-  fecha_registro: Timestamp;
-}
