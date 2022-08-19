c15b753ff429013d3fea536a5648e9235e6aa668
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-08-19 10:39:17 -0500

    formatear documento

--- src/components/FirebaseDataTable/FirebaseDataTable.tsx
+++ src/components/FirebaseDataTable/FirebaseDataTable.tsx
@@ -87,12 +87,16 @@ export const FirebaseDataTable = (props: Props) => {
   }, [dir]);
 
   return (
-    <div className=''>
+    <div className='w-full'>
       {loading ? (
-        <p ><Spinner /></p>
+        <p>
+          <Spinner />
+        </p>
       ) : (
         <>
-          <div className={`overflow-x-auto w-[99.%] h-full absolute ${style.scroll}`} >
+          <div
+            className={`overflow-x-auto w-full h-full absolute ${style.scroll}`}
+          >
             <table className={`table w-full border text-sm mb-4 `}>
               <thead className='border'>
                 <tr className='border'>
@@ -101,63 +105,62 @@ export const FirebaseDataTable = (props: Props) => {
                   )}
                 </tr>
               </thead>
-              <tbody >
+              <tbody>
                 {React.Children.toArray(
                   data.map((d) => <props.RowComponent values={d} />)
                 )}
               </tbody>
             </table>
-                      <div className='flex justify-end items-center'>
-            <div className='flex items-center mr-3'>
-              <p className='mr-2'>Numero de filas</p>
-              <select
-                className='select select-sm'
-                onChange={(e) => setPerPage(parseInt(e.target.value))}
-                defaultValue={perPage}
-              >
-                <option value='10'> 10 </option>
-                <option value='15'> 15 </option>
-                <option value='20'> 20 </option>
-                <option value='40'> 40 </option>
-              </select>
-            </div>
+            <div className='flex justify-end items-center px-3'>
+              <div className='flex items-center mr-3'>
+                <p className='mr-2'>Numero de filas</p>
+                <select
+                  className='select select-sm'
+                  onChange={(e) => setPerPage(parseInt(e.target.value))}
+                  defaultValue={perPage}
+                >
+                  <option value='10'> 10 </option>
+                  <option value='15'> 15 </option>
+                  <option value='20'> 20 </option>
+                  <option value='40'> 40 </option>
+                </select>
+              </div>
 
-            <p className='mr-3'>
-              <span className='mr-2'>{page * perPage - (perPage - 1)}</span>
-              <span className='mr-2'>-</span>
-              <span className='mr-2'>{page * perPage}</span>
-              <span className='mr-2'>de </span>
-              <span>{Math.ceil(total / perPage)} </span>
-            </p>
+              <p className='mr-3 text-sm'>
+                <span className='mr-2'>{page * perPage - (perPage - 1)}</span>
+                <span className='mr-2'>-</span>
+                <span className='mr-2'>{page * perPage}</span>
+                <span className='mr-2'>de </span>
+                <span>{Math.ceil(total / perPage)} </span>
+              </p>
 
-            <div className='btn-group'>
-              <button
-                className='btn btn-sm bg-white border-blue-200 text-slate-900 font-bold hover:bg-blue-500 hover:border-white'
-                onClick={() => {
-                  setPage((actualPage) => actualPage - 1);
-                  setDir('prev');
-                }}
-                disabled={page === 1}
-              >
-                «
-              </button>
-              <button className='btn btn-sm bg-white border-blue-200 text-slate-900 font-bold hover:bg-blue-500 hover:border-white'>
-                {page}
-              </button>
-              <button
-                className='btn btn-sm bg-white border-blue-200 text-slate-900 font-bold hover:bg-blue-500 hover:border-white'
-                onClick={() => {
-                  setPage((actualPage) => actualPage + 1);
-                  setDir('next');
-                }}
-                disabled={page === Math.ceil(total / perPage)}
-              >
-                »
-              </button>
+              <div className='btn-group'>
+                <button
+                  className='btn btn-sm bg-white border-blue-200 text-slate-900 font-bold hover:bg-blue-500 hover:border-white'
+                  onClick={() => {
+                    setPage((actualPage) => actualPage - 1);
+                    setDir('prev');
+                  }}
+                  disabled={page === 1}
+                >
+                  «
+                </button>
+                <button className='btn btn-sm bg-white border-blue-200 text-slate-900 font-bold hover:bg-blue-500 hover:border-white'>
+                  {page}
+                </button>
+                <button
+                  className='btn btn-sm bg-white border-blue-200 text-slate-900 font-bold hover:bg-blue-500 hover:border-white'
+                  onClick={() => {
+                    setPage((actualPage) => actualPage + 1);
+                    setDir('next');
+                  }}
+                  disabled={page === Math.ceil(total / perPage)}
+                >
+                  »
+                </button>
+              </div>
             </div>
           </div>
-          </div>
-
         </>
       )}
     </div>
