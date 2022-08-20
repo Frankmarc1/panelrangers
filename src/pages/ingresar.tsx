cb263004b0b1a783e7960883961c31254aa44cdc
Termux-Master93 <brisantur8034@gmail.com>
2022-08-19 19:47:20 -0500

    FlotingInput para inputs de login

--- src/pages/ingresar.tsx
+++ src/pages/ingresar.tsx
@@ -5,6 +5,7 @@ import { Toaster, toast } from 'react-hot-toast';
 import { handleSignIn } from '../app/signIn/helpers/SignIn';
 import authErrors from '../app/signIn/types/authErrors';
 
+
 const SignIn = () => {
   const refEmail = useRef<HTMLInputElement>(null);
   const refPass = useRef<HTMLInputElement>(null);
@@ -55,24 +56,35 @@ const SignIn = () => {
         reverseOrder={false}
         toastOptions={{ duration: 5000 }}
       />
-      <div className='h-80 w-80 flex justify-center items-center flex-col bg-base-100  rounded shadow  p-3'>
-        <h1 className='text-2xl mb-4 text-center'>Ingresar</h1>
+      <div className='h-80 w-80 flex justify-center items-center flex-col bg-base-100 border-2 border-cyan-50 rounded-xl shadow  p-3'>
+        <h1 className='text-2xl mb-5 text-center'>Ingresar</h1>
         <form onSubmit={handleForm}>
-          <input
-            type={'email'}
-            placeholder='Ingrese email'
-            className='input input-sm w-full mb-4'
-            name='email'
-            ref={refEmail}
-          />
-          <input
-            type={'password'}
-            placeholder='Ingrese contraseña'
-            className='input input-sm w-full mb-4'
-            name='pass'
-            ref={refPass}
-          />
-          <button className='w-full btn btn-primary btn-sm' disabled={loading}>
+          <div className="relative w-full">
+            <input
+              type={'email'}
+              placeholder='Ingrese email'
+              className='input input-md w-full mb-4 block px-2.8 pb-2.5 pt-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder=" " '
+              name='email'
+              ref={refEmail}
+              required
+            />
+            <label htmlFor="email" className="absolute text-md text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Ingrese email</label>
+          </div>
+
+          <div className="relative w-72">
+            <input
+              type={'password'}
+              placeholder='Ingrese contraseña'
+              className='input input-md w-full mb-4 block px-2.5 pb-2.5 pt-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder=" " '
+              name='pass'
+              id="pass"
+              ref={refPass}
+              required
+            />
+            <label htmlFor="pass" className="absolute text-md text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Ingrese contraseña</label>
+          </div>
+
+          <button className='w-full btn btn-primary btn-md ' disabled={loading}>
             {loading ? 'Cargando...' : 'Ingresar'}
           </button>
         </form>
