752c7a764bb56143d45d39dbf12d46eea2212ad0
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-08-25 09:49:55 -0500

    actualizar query para obtener agencias

--- src/pages/dashboard/agencias/index.tsx
+++ src/pages/dashboard/agencias/index.tsx
@@ -1,15 +1,13 @@
-import { collection } from 'firebase/firestore';
+import { collection, orderBy, query } from 'firebase/firestore';
+
 import { RowAgencies } from '../../../app/agencies/RowAgencies';
 import { FirebaseDataTable } from '../../../components/FirebaseDataTable/FirebaseDataTable';
 import { db_client } from '../../../firebase/client';
 import { Dashboard } from '../../../layout/Dashboard/Dashboard';
 
-const colRef = collection(db_client, 'empresas_agencia');
-
 const Agencies = () => {
   return (
     <Dashboard>
-      <div className='h-[35.2rem] relative'>
       <FirebaseDataTable
         headers={[
           'NOMBRE',
@@ -20,9 +18,8 @@ const Agencies = () => {
           'ACCIONES',
         ]}
         RowComponent={RowAgencies}
-        qi={colRef}
+        qi={query(collection(db_client, 'empresas_agencia'), orderBy('id'))}
       />
-      </div>
     </Dashboard>
   );
 };
