22ef372f4be791ab313c8fba6cd55fb984394bec
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-08-26 09:16:41 -0500

    reescribir collection reference

--- src/pages/agencias/[idAgency]/agentes/index.tsx
+++ src/pages/agencias/[idAgency]/agentes/index.tsx
@@ -1,37 +1,35 @@
-
 import { collection, doc, orderBy, query, where } from 'firebase/firestore';
 import { useRouter } from 'next/router';
-import {RowAgent} from '../../../../app/agencies/agent/RowAgent';
+
+import { RowAgent } from '../../../../app/agencies/agent/RowAgent';
 import { FirebaseDataTable } from '../../../../components/FirebaseDataTable/FirebaseDataTable';
 import { db_client } from '../../../../firebase/client';
 import { Dashboard } from '../../../../layout/Dashboard/Dashboard';
 
-const Agentes = () => {
+const Agents = () => {
   const router = useRouter();
   const { idAgency } = router.query;
+
   return (
     <Dashboard>
       <FirebaseDataTable
         RowComponent={RowAgent}
-        headers={[
-          'Logo',
-          'Nombre',
-          'Dirreción',
-          'Estado',
-          'Acciones',
-        ]}
+        headers={['Logo', 'Nombre', 'Dirreción', 'Estado', 'Acciones']}
         qi={query(
-          collection(db_client, 'agencia_empresas'),
+          collection(
+            db_client,
+            `empresas_agencia/${idAgency}/agencia_empresas`
+          ),
           where(
-            'reference_agencia',        
+            'reference_agencia',
             '==',
             doc(db_client, `empresas_agencia/${idAgency}`)
           ),
-          orderBy("nombre")
+          orderBy('nombre', 'asc')
         )}
       />
     </Dashboard>
   );
 };
 
-export default Agentes;
+export default Agents;
