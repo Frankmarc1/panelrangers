4aebeeb1507a85db88cd675c91b703e27dd5be60
Termux-Master93 <86426464+Termux-Master93@users.noreply.github.com>
2022-08-31 13:46:59 -0500

    agregar listado dentro de card

--- src/pages/agencias/index.tsx
+++ src/pages/agencias/index.tsx
@@ -1,4 +1,6 @@
 import { collection, orderBy, query } from 'firebase/firestore';
+import Link from 'next/link';
+import { IoIosAddCircle } from "react-icons/io";
 
 import { RowAgencies } from '../../app/agencies/RowAgencies';
 import { FirebaseDataTable } from '../../components/FirebaseDataTable/FirebaseDataTable';
@@ -8,18 +10,31 @@ import { Dashboard } from '../../layout/Dashboard/Dashboard';
 const Agencies = () => {
   return (
     <Dashboard>
-      <FirebaseDataTable
-        headers={[
-          'NOMBRE',
-          'DEPARTAMENTO',
-          'DIRECCION',
-          'ESTADO',
-          'FECHA DE REGISTRO',
-          'ACCIONES',
-        ]}
-        RowComponent={RowAgencies}
-        qi={query(collection(db_client, 'empresas_agencia'), orderBy('id'))}
-      />
+      <div className='card  bg-white boder'>
+      <div className='card-header  mx-[1rem] py-2'>
+          <div className='flex justify-between'>
+            <h3 className="card-title">Lista de Agentes</h3>
+            <Link href={`#`}>
+              <button type="button" className="btn btn-primary btn-sm"><span className='text-xl mr-2'><IoIosAddCircle /></span>Agregar Agencias</button>
+
+            </Link>
+          </div>
+        </div>
+        <div className='mx-[1rem] mb-2'>
+          <FirebaseDataTable
+            headers={[
+              'NOMBRE',
+              'DEPARTAMENTO',
+              'DIRECCION',
+              'ESTADO',
+              'FECHA DE REGISTRO',
+              'ACCIONES',
+            ]}
+            RowComponent={RowAgencies}
+            qi={query(collection(db_client, 'empresas_agencia'), orderBy('id'))}
+          />
+        </div>
+      </div>
     </Dashboard>
   );
 };
