960ca912d6697537deca08852068b3c2e643ab2a
Termux-Master93 <86426464+Termux-Master93@users.noreply.github.com>
2022-09-01 10:58:49 -0500

    crear formulario de sanciones

--- src/pages/agencias/[idAgency]/sanciones/index.tsx
+++ src/pages/agencias/[idAgency]/sanciones/index.tsx
@@ -4,6 +4,8 @@ import { RowSanctions } from '../../../../app/agencies/sanction/RowSanctions';
 import { FirebaseDataTable } from '../../../../components/FirebaseDataTable/FirebaseDataTable';
 import { db_client } from '../../../../firebase/client';
 import { Dashboard } from '../../../../layout/Dashboard/Dashboard';
+import Link from 'next/link';
+import { IoIosAddCircle } from "react-icons/io";
 
 const Sanctions = () => {
   const router = useRouter();
@@ -11,16 +13,28 @@ const Sanctions = () => {
 
   return (
     <Dashboard>
-      <FirebaseDataTable
-        RowComponent={RowSanctions}
-        headers={['Titulo', 'Detalles', 'Puntos', 'Tipo','Fecha de Inicio','Fecha Final','Estado','Acciones']}
-        qi={query(
-          collection(
-            db_client,
-            `empresas_agencia/${idAgency}/recompensas_y_sanciones`
-          ),orderBy('titulo')
-        )}
-      />
+      <div className='card bg-white border'>
+        <div className='card-header  mx-[1rem] py-2'>
+          <div className='flex justify-between'>
+            <h3 className='card-title'>Listado de recompensas y sanciones</h3>
+            <Link href={`/agencias/${idAgency}/sanciones/add`}>
+              <button type="button" className="btn btn-primary btn-sm"><span className='text-xl mr-2'><IoIosAddCircle /></span>Agregar Recompensas o Sanciones</button>
+            </Link>
+          </div>
+        </div>
+        <div className='mx-[1rem]'>
+          <FirebaseDataTable
+            RowComponent={RowSanctions}
+            headers={['Titulo', 'Detalles', 'Puntos', 'Tipo', 'Fecha de Inicio', 'Fecha Final', 'Estado', 'Acciones']}
+            qi={query(
+              collection(
+                db_client,
+                `empresas_agencia/${idAgency}/recompensas_y_sanciones`
+              ), orderBy('titulo')
+            )}
+          />
+        </div>
+      </div>
     </Dashboard>
   );
 };
