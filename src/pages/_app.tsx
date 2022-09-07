fce58c059573eb4e539eda90e98a9e087197be6d
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-09-07 09:57:45 -0500

    borrar configuracion de font awesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,6 @@
-import { config } from '@fortawesome/fontawesome-svg-core';
 import type { AppProps } from 'next/app';
 
 import '../../styles/globals.css';
-
-config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
