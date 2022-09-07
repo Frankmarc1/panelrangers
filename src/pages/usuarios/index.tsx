02b220a6446e518152528083a1379d815062856f
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-09-07 10:06:12 -0500

    agregar ruta para redirigir  a roles

--- src/pages/usuarios/index.tsx
+++ src/pages/usuarios/index.tsx
@@ -3,8 +3,9 @@ import { collection } from 'firebase/firestore';
 import { RowUsers } from '../../app/users/RowUsers';
 import { FirebaseDataTable } from '../../components/FirebaseDataTable/FirebaseDataTable';
 import { db_client } from '../../firebase/client';
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaPlusCircle } from 'react-icons/fa';
 import Head from 'next/head';
+import Link from 'next/link';
 const colRef = collection(db_client, 'rangers_masters');
 
 const Users = () => {
@@ -14,10 +15,16 @@ const Users = () => {
         <title>Usuarios</title>
       </Head>
       <div>
-        <button className='btn btn-success gap-2 btn-sm mb-2'>
-          <FaPlus />
-          Crear usuario
-        </button>
+        <div>
+          <button className='btn btn-success gap-2 btn-sm mb-2 mr-2'>
+            <FaPlusCircle />
+            Crear usuario
+          </button>
+          <Link href={'/roles'}>
+            <a className='text-md text-blue-700 font-bold'>Roles</a>
+          </Link>
+        </div>
+
         <FirebaseDataTable
           headers={['D.N.I', 'NOMBRE Y APELLIDOS', 'ROL', 'ACCIONES']}
           RowComponent={RowUsers}
