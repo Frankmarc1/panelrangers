c53c67445ea7cad9d2845acc64f0a8926c4d1ff8
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-09-07 10:31:39 -0500

    crear pagina agregar agente

---
+++ src/pages/agencias/[idAgency]/agentes/agregar.tsx
@@ -0,0 +1,12 @@
+import { FormAgent } from '../../../../app/agencies/agent/FormAgent';
+import { Dashboard } from '../../../../layout/Dashboard/Dashboard';
+
+const Add = () => {
+  return (
+    <Dashboard>
+      <FormAgent />
+    </Dashboard>
+  );
+};
+
+export default Add;
