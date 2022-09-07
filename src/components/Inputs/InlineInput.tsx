43e9225b2488d985c0029f9e4ce1fd605e51b434
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-09-07 10:03:11 -0500

    incluir funcion para guardar datos

--- src/components/Inputs/InlineInput.tsx
+++ src/components/Inputs/InlineInput.tsx
@@ -1,18 +1,37 @@
 import { useRef, useState } from 'react';
+import toast from 'react-hot-toast';
 import { FaPenAlt, FaSave, FaTimesCircle } from 'react-icons/fa';
 
 interface Props {
   value: string;
+  onSave: (a: string, ...b: any) => Promise<any>;
 }
 
-export const InlineInput = ({ value }: Props) => {
+export const InlineInput = ({ value, onSave }: Props) => {
   const [edit, setEdit] = useState(false);
-  const ref = useRef();
+  const [loading, setLoading] = useState(false);
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleSave = async () => {
+    setLoading(true);
+    const value = ref.current?.value;
+
+    if (!value) {
+      toast.error('Valor no referenciado.');
+
+      return;
+    }
+
+    await onSave(value);
+
+    setLoading(false);
+    setEdit(false);
+  };
 
   return (
     <div className='w-auto'>
       {!edit ? (
-        <p className='flex items-center'>
+        <p className='grid grid-cols-2'>
           {' '}
           {value}{' '}
           <FaPenAlt
@@ -21,18 +40,25 @@ export const InlineInput = ({ value }: Props) => {
           />{' '}
         </p>
       ) : (
-        <div className='flex w-auto items-center'>
+        <div className='flex items-center'>
           <input
             type='text'
-            className='input input-bordered w-5/12 mr-2 input-sm'
+            className='input input-bordered w-1/5 mr-2 input-sm'
             defaultValue={value}
+            ref={ref}
           />
           <div className='flex'>
-            <FaSave className='text-primary mr-2' />
-            <FaTimesCircle
-              className='text-error'
-              onClick={() => setEdit(false)}
-            />
+            {loading ? (
+              <button className='btn btn-sm btn-ghost loading'></button>
+            ) : (
+              <>
+                <FaSave className='text-primary mr-2' onClick={handleSave} />
+                <FaTimesCircle
+                  className='text-error'
+                  onClick={() => setEdit(false)}
+                />
+              </>
+            )}
           </div>
         </div>
       )}
