24218c495085edc62e096258327f62d6033595f2
Termux-Master93 <brisantur8034@gmail.com>
2022-09-09 11:55:33 -0500

    cabiar diseño de dropwdow por sidebar

--- src/layout/Dashboard/Dashboard.tsx
+++ src/layout/Dashboard/Dashboard.tsx
@@ -1,11 +1,15 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
+
 import { Navbar } from './components/Navbar';
+import { FaBars} from 'react-icons/fa';
+import HeaderNav from './components/HeaderNav';
 
 interface props {
   children: ReactNode | ReactNode[];
 }
 export const Dashboard = ({ children }: props) => {
+  const [show, setShow] = useState(true)
   return (
     <>
       <Toaster
@@ -14,10 +18,31 @@ export const Dashboard = ({ children }: props) => {
         reverseOrder={false}
         toastOptions={{ duration: 5000 }}
       />
-      <div className='h-screen'>
-        <Navbar />
-        <main className='p-3 w-full'>{children}</main>
-      </div>
+     
+      <div className="min-h-screen w-full bg-gray-100 text-gray-700" x-data="layout">
+        <header className="flex w-full items-center justify-between border-b-2 border-gray-200 bg-white p-2">
+          <div className="flex items-center space-x-2">
+            <label tabIndex={0} onClick={() => setShow(!show)} className=' btn btn-ghost btn-circle'>
+              <FaBars />
+            </label>
+          </div>
+
+          <HeaderNav />
+          
+        </header >
+
+        <div className="flex">
+          {
+            show ?
+            <Navbar />: null
+
+          }
+       
+          <main className='p-3 w-full'>{children}</main>
+        </div>
+      </div >
+
+
     </>
   );
 };
--- src/layout/Dashboard/components/Navbar.tsx
+++ src/layout/Dashboard/components/Navbar.tsx
@@ -1,12 +1,91 @@
 import Link from 'next/link';
-import { FaBars, FaList } from 'react-icons/fa';
+import React from 'react';
+import { FaUserTie,FaMotorcycle } from "react-icons/fa";
+import { BsFillPersonCheckFill } from "react-icons/bs";
+import { ImOffice } from "react-icons/im";
+import { HiOfficeBuilding } from "react-icons/hi";
 
 import { useRouteMatch } from '../../../hooks/useRouteMatch';
-
 export const Navbar = () => {
   const searchPath = useRouteMatch();
 
   return (
+    <aside className="flex w-72 flex-col space-y-2 border-r-2 border-gray-200 bg-white p-2 min-h-[36rem] "
+      x-show="asideOpen">
+
+      <Link href={'/usuarios'}>
+        <a
+          className={`
+          flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600
+          ${searchPath('usuarios') ? 'text-blue-500 font-bold' : ''}`
+          }
+        >
+          <div className='mr-3 text-slate-600 text-xl'><FaUserTie/></div>
+          {' '}
+          Usuarios
+          {' '}
+        </a>
+      </Link>
+
+      <Link href='/subs'>
+        <a
+          className={`
+          flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600
+           ${searchPath('subs') ? 'text-blue-500 font-bold' : ''}`
+          }
+        >
+          <div className='mr-3 text-slate-600 text-xl'><BsFillPersonCheckFill/></div>
+          {' '}
+          Subscriciones{' '}
+        </a>
+      </Link>
+
+      <Link href='/motorizados'>
+        <a
+          className={`
+            flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600
+            ${searchPath('motorizados') ? 'text-blue-500 font-bold' : ''}`
+          }
+        >
+          <div className='mr-3 text-slate-600 text-xl'><FaMotorcycle /></div>
+          {' '}
+          Motorizados{' '}
+        </a>
+      </Link>
+
+      <Link href='/agencias'>
+        <a
+          className={`
+          flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600
+           ${searchPath('agencias') ? 'text-blue-500 font-bold' : ''}`
+          }
+        >
+          <div className='mr-3 text-slate-600 text-xl'><ImOffice /></div>
+          {' '}
+          Agencias{' '}
+        </a>
+      </Link>
+
+      <Link href='/empresas'>
+        <a
+          className={`
+            flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600
+            ${searchPath('empresas') ? 'text-blue-500 font-bold' : ''}`
+          }
+          
+        >
+          <div className='mr-3 text-slate-600 text-xl'><HiOfficeBuilding /></div>
+          {' '}
+          Empresas{' '}
+        </a>
+      </Link>
+
+
+    </aside>
+
+  );
+
+  /*return (
     <nav className='navbar bg-base-100 w-full'>
       <div className='navbar-start'>
         <div className='dropdown'>
@@ -106,6 +185,9 @@ export const Navbar = () => {
           </ul>
         </div>
       </div>
-    </nav>
-  );
+       </nav>
+                
+                
+              
+  );*/
 };
