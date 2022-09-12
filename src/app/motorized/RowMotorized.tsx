d5cec89053226799af4511f26e1f7abe3f8e8304
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-09-12 11:58:17 -0500

    crear funciones desubscribir, y eliminar motorizados

--- src/app/motorized/RowMotorized.tsx
+++ src/app/motorized/RowMotorized.tsx
@@ -1,9 +1,10 @@
 import { Motorized } from '../../types/motorized';
 import { DateTime } from 'luxon';
-import { FaPen, FaUserAltSlash } from 'react-icons/fa';
-import { ImProfile } from "react-icons/im";
+import { FaArrowRight, FaPen, FaUserAltSlash } from 'react-icons/fa';
+import { ImProfile } from 'react-icons/im';
 import Link from 'next/link';
-
+import { deleteMotorized } from './helpers/deleteMotorized';
+import { unsubscribeMotorized } from './helpers/unsubscribeMotorized';
 
 export const RowMotorized = ({ values }: { values: Motorized }) => {
   return (
@@ -22,30 +23,51 @@ export const RowMotorized = ({ values }: { values: Motorized }) => {
           )}{' '}
         </td>
         <td className=''>
-          <Link href={`motorizados/${values.id}/editar`}>
-            <button className='btn btn-success btn-sm mr-3' >
-              <FaPen />
-            </button>
-          </Link>
+          {values.status == 'ELIMINADO' ? (
+            <p className='text-xs text-error font-bold'> ELIMINADO </p>
+          ) : (
+            <>
+              <Link href={`motorizados/${values.id}/editar`}>
+                <button className='btn btn-success btn-sm mr-3'>
+                  <FaPen />
+                </button>
+              </Link>
 
-          <button className='btn btn-error btn-sm mr-3'>
-            <FaUserAltSlash />
-          </button>
+              <Link
+                href={`motorizados/${values.id}/cartilla`}
+                as={`motorizados/${values.id}/cartilla`}
+              >
+                <a>
+                  <button className='btn btn-info btn-sm mr-3'>
+                    <ImProfile />
+                  </button>
+                </a>
+              </Link>
 
-          <Link
-            href={`motorizados/${values.id}/cartilla`}
-            as={`motorizados/${values.id}/cartilla`}
-          >
-            <a>
-              <button className='btn btn-info btn-sm'>
-                <ImProfile />
+              <button
+                className='btn btn-warning btn-sm mr-3'
+                onClick={() => {
+                  unsubscribeMotorized(values.id);
+                }}
+              >
+                <FaArrowRight />
               </button>
-            </a>
-          </Link>
 
+              <button
+                className='btn btn-error btn-sm'
+                onClick={() =>
+                  deleteMotorized(
+                    values.id,
+                    `${values.profile.name} ${values.profile.lastName} `
+                  )
+                }
+              >
+                <FaUserAltSlash />
+              </button>
+            </>
+          )}
         </td>
       </tr>
-         
     </>
   );
 };
