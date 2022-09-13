e30b5c706a45982b40873764760c1787e9c801b6
Termux-Master93 <86426464+Termux-Master93@users.noreply.github.com>
2022-09-13 13:51:48 -0500

    agregar botton de crear categoria principal

--- src/pages/categorias_principales/index.tsx
+++ src/pages/categorias_principales/index.tsx
@@ -1,22 +1,36 @@
+import { db_client } from "../../firebase/client";
+import { query, collection, orderBy } from "firebase/firestore";
+import { FaPlus } from 'react-icons/fa';
+import Link from 'next/link';
+
 import { Dashboard } from "../../layout/Dashboard/Dashboard";
 import { FirebaseDataTable } from "../../components/FirebaseDataTable/FirebaseDataTable";
-import { db_client } from "../../firebase/client";
-import { query,collection,orderBy } from "firebase/firestore";
+
 import { RowMaincategory } from "../../app/mainCategory/RowMainCategory";
-const MainCategorys=()=>{
-    return(
+const MainCategorys = () => {
+    return (
         <Dashboard>
-                    <FirebaseDataTable
-                    headers={[
-                        'Categoria',
-                        'Imagen',
-                        'Acciones'
-                    ]}
-                    RowComponent={RowMaincategory}
-                    qi={query(collection(db_client, 'main_categories'), orderBy('id'))}
-                />
+            <Link href={`categorias_principales/add`}  >
+            <a>
+                <button className='btn btn-primary gap-2 btn-sm mb-2'>
+                    <FaPlus />
+                    Agregar Categoria
+                </button>
+            </a>
+            </Link>
+            
+          
+            <FirebaseDataTable
+                headers={[
+                    'Categoria',
+                    'Imagen',
+                    'Acciones'
+                ]}
+                RowComponent={RowMaincategory}
+                qi={query(collection(db_client, 'main_categories'), orderBy('id'))}
+            />
         </Dashboard>
-      
+
     )
 }
 export default MainCategorys;
