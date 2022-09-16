02c0f5e91da6bd8c4404a28843ba5df02a3ef4b3
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-09-16 09:28:48 -0500

    diseñar unicamente un sidebar

--- src/layout/Dashboard/Dashboard.tsx
+++ src/layout/Dashboard/Dashboard.tsx
@@ -1,15 +1,62 @@
+import Link from 'next/link';
 import { ReactNode, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
-
-import { Navbar } from './components/Navbar';
-import { FaBars} from 'react-icons/fa';
-import HeaderNav from './components/HeaderNav';
+import {
+  FaBuilding,
+  FaClipboardList,
+  FaCopy,
+  FaHome,
+  FaListUl,
+  FaMotorcycle,
+  FaRandom,
+  FaRegBuilding,
+  FaSquare,
+  FaUsers,
+} from 'react-icons/fa';
+import { IconType } from 'react-icons/lib';
+import { useRouteMatch } from '../../hooks/useRouteMatch';
 
 interface props {
   children: ReactNode | ReactNode[];
 }
+
+interface Menu {
+  title: string;
+  href: string;
+  icon: JSX.Element;
+  gap?: boolean;
+}
 export const Dashboard = ({ children }: props) => {
-  const [show, setShow] = useState(true)
+  const [open, setOpen] = useState(true);
+  const searchPath = useRouteMatch();
+
+  const Menus: Menu[] = [
+    { title: 'Usuarios', href: '/usuarios', icon: <FaUsers /> },
+    { title: 'Motorizados', href: '/motorizados', icon: <FaMotorcycle /> },
+    { title: 'Agencias', href: '/agencias', icon: <FaHome /> },
+    {
+      title: 'Empresas',
+      href: '/empresas',
+      icon: <FaBuilding />,
+      gap: true,
+    },
+    {
+      title: 'Sectores Economicos',
+      href: '/sectores-economicos',
+      icon: <FaListUl />,
+    },
+    {
+      title: 'Categorias Principales',
+      href: '/categorias_principales',
+      icon: <FaClipboardList />,
+    },
+    {
+      title: 'Secciones',
+      href: '/secciones',
+      icon: <FaSquare />,
+    },
+  ];
+
   return (
     <>
       <Toaster
@@ -18,31 +65,57 @@ export const Dashboard = ({ children }: props) => {
         reverseOrder={false}
         toastOptions={{ duration: 5000 }}
       />
-     
-      <div className="min-h-screen w-full bg-gray-100 text-gray-700" x-data="layout">
-        <header className="flex w-full items-center justify-between border-b-2 border-gray-200 bg-white p-2">
-          <div className="flex items-center space-x-2">
-            <label tabIndex={0} onClick={() => setShow(!show)} className=' btn btn-ghost btn-circle'>
-              <FaBars />
-            </label>
+
+      <div className='flex'>
+        <div
+          className={` ${
+            open ? 'w-72' : 'w-20 '
+          } bg-dark-purple h-screen p-5  pt-8 relative duration-300`}
+        >
+          <img
+            src='/control.png'
+            className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+           border-2 rounded-full  ${!open && 'rotate-180'}`}
+            onClick={() => setOpen(!open)}
+          />
+
+          <div className='flex gap-x-4 items-center'>
+            <FaRandom
+              className={`cursor-pointer duration-500  text-white ${
+                open && 'rotate-[360deg]'
+              }`}
+            />
+            <h1
+              className={`text-white origin-left font-medium text-xl duration-200 ${
+                !open && 'scale-0'
+              }`}
+            >
+              RangersDashboard
+            </h1>
           </div>
-
-          <HeaderNav />
-          
-        </header >
-
-        <div className="flex">
-          {
-            show ?
-            <Navbar />: null
-
-          }
-       
-          <main className='p-3 w-full'>{children}</main>
+          <ul className='pt-6'>
+            {Menus.map((menu, index) => (
+              <Link href={menu.href}>
+                <a
+                  key={index}
+                  className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
+              ${menu.gap ? 'mt-9' : 'mt-2'} ${
+                    searchPath(menu.href.split('/')[1]) && 'bg-light-white'
+                  } `}
+                >
+                  {menu.icon}
+                  <span
+                    className={`${!open && 'hidden'} origin-left duration-200`}
+                  >
+                    {menu.title}
+                  </span>
+                </a>
+              </Link>
+            ))}
+          </ul>
         </div>
-      </div >
-
-
+        <div className='h-screen flex-1 p-7'>{children}</div>
+      </div>
     </>
   );
 };
