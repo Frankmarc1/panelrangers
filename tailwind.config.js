ab4ccd21e24ec5ccb9f710da245794b4af6c8c37
edgarizagni31 <ochoaEdgar3108@gmail.com>
2022-09-16 09:27:30 -0500

    agregar nuevos colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-purple': '#081A51',
+        'light-white': 'rgba(255,255,255,0.17)',
+      },
+    },
   },
   plugins: [require('daisyui')],
 };
