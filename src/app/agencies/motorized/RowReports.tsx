ba86471f7bcea36a0f15ab6b642dbe3e67c82151
Termux-Master93 <86426464+Termux-Master93@users.noreply.github.com>
2022-09-16 13:38:53 -0500

    ajustar los tamaños de los td de la tabla reportes para ganar espacio

--- src/app/agencies/motorized/RowReports.tsx
+++ src/app/agencies/motorized/RowReports.tsx
@@ -9,22 +9,29 @@ import { savePercent } from './helpers/savePercent';
 
 export const RowReports = ({ values }: { values: Report }) => {
   const { idMotorized } = useRouter().query as Params;
+
   return (
     <tr>
       <td> {values.cantidad_pedidos} </td>
       <td> {values.nombreReporte} </td>
-      <td>
-        <InlineInput
-          value={values.observacion}
-          onSave={async (value) =>
-            await saveObs(value, idMotorized, values.idReporte)
-          }
-        />
+      <td >
+        {
+          values.observacion
+            ? <div className='pr-5'>
+              <InlineInput
+                value={values.observacion}
+                onSave={async (value) =>
+                  await saveObs(value, idMotorized, values.idReporte)
+                }
+              />
+            </div>
+            : null
+        }
       </td>
       <td> {currencyFormat(values.deuda)} </td>
       <td> {currencyFormat(values.deuda)} </td>
       <td> {currencyFormat(values.deuda)} </td>
-      <td>
+      <td className='mr-0 pr-0'>
         <InlineInput
           value={formatPercent(values.porcentaje)}
           onSave={async (value) =>
@@ -32,10 +39,11 @@ export const RowReports = ({ values }: { values: Report }) => {
           }
         />
       </td>
-      <td> {values.estado} </td>
+      <td >
+        <p className='ml-4'> {values.estado}</p> </td>
       <td>
         {values.registro_horas && (
-          <div className='badge badge-primary mr-2'>
+          <div className='badge badge-primary'>
             {values.registro_horas
               .filter((register) => register.tipo === 'Comisión')
               .map((register) => register.horas)
