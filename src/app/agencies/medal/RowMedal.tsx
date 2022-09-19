3e06f8e3dee2d695bae0a3e2062d101c196e56b3
Termux-Master93 <86426464+Termux-Master93@users.noreply.github.com>
2022-09-19 13:54:54 -0500

    agregar ruta pra modificar medalla

--- src/app/agencies/medal/RowMedal.tsx
+++ src/app/agencies/medal/RowMedal.tsx
@@ -1,6 +1,13 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { FaPen } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+
 import { Medal } from "../../../types/medal";
 
 export const RowMedal = ({ values }: { values: Medal }) => {
+    const router = useRouter();
+    const { idAgency } = router.query;
     return (
         <tr>
             <td>
@@ -9,7 +16,21 @@ export const RowMedal = ({ values }: { values: Medal }) => {
             <td>
                 {values.nombre}</td>
             <td><p className="w-[25rem] truncate">{values.descripcion} </p> </td>
-            <td></td>
+            <td>
+                <Link href={`/agencias/${idAgency}/medallas/${values.id}`}>
+                    <button className='btn btn-success btn-sm mr-3'>
+                        <FaPen />
+                    </button>
+                </Link>
+
+                <Link href={`/agencias/`}>
+                    <button className='btn btn-error btn-sm mr-3'>
+                        <AiFillDelete />
+                    </button>
+                </Link>
+            </td>
+
+
         </tr>
     );
 };
