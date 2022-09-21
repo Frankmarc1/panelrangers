8cfd846b2da4bc8e1d3812374033d7d675bdbfde
Termux-Master93 <86426464+Termux-Master93@users.noreply.github.com>
2022-09-21 13:59:20 -0500

    crear la pagina para modificar categorias principales

---
+++ src/pages/categorias_principales/[idCategory]/index.tsx
@@ -0,0 +1,7 @@
+import { FormMainCategorys } from "../../../components/MainCatories/FormMainCatories";
+const modifyMainCategory=()=>{
+    return (  
+            <FormMainCategorys/>
+    );
+}
+export default modifyMainCategory;
