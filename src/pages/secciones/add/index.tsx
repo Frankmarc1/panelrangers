615ae1347bf34ec00bc87481685b4c90d3a065dd
Termux-Master93 <86426464+Termux-Master93@users.noreply.github.com>
2022-09-22 13:39:55 -0500

    crear pagina para crear secciones

---
+++ src/pages/secciones/add/index.tsx
@@ -0,0 +1,11 @@
+import { Dashboard } from "../../../layout/Dashboard/Dashboard";
+import FormSections from "../../../app/sections/FormSections";
+const AddSection = () => {
+    return (
+        <Dashboard>
+            <FormSections />
+        </Dashboard>
+
+    );
+}
+export default AddSection;
