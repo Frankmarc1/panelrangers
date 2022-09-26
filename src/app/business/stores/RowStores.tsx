48d2033e9b3b29a8d6ef957b4a24bd19d031f33e
Termux-Master93 <brisantur8034@gmail.com>
2022-09-26 09:04:43 -0500

    incluir estado de verificacion y esatdo de atencion en listado de tiendas

--- src/app/business/stores/RowStores.tsx
+++ src/app/business/stores/RowStores.tsx
@@ -1,10 +1,12 @@
-import { getDoc } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { ChangeEvent, useEffect, useState } from 'react';
 import Link from 'next/link';
-
+import { useRouter } from 'next/router';
+import { db_client } from '../../../firebase/client';
+import { checkingCommerce } from '../../../components/Helpers/chekingComerce';
+import { toast } from 'react-hot-toast';
+import { StatusHandler } from '../../../common/statusHndler/StatusHandler';
 import { Store } from '../../../types/store';
-import router from 'next/router';
-
 
 interface Nombres {
     name: string;
@@ -13,6 +15,35 @@ interface Nombres {
 export const RowStores = ({ values }: { values: Store }) => {
     const [nameSector, setNameSector] = useState('');
     const [loading, setLoading] = useState(true);
+    const [loading2, setLoading2] = useState<boolean>(false);
+
+    const router = useRouter();
+    const { idBusiness } = router.query
+
+    const handleStatusChange = async (e: ChangeEvent<HTMLInputElement>) => {
+        setLoading(true);
+        await checkingCommerce(
+            `/empresas/${idBusiness}/comercios/${values.id}`,
+            e.target.checked
+        );
+        setLoading(false);
+    };
+
+    const handleStatus = async (e: ChangeEvent<HTMLInputElement>) => {
+        setLoading2(true);
+        await updateDoc(doc(db_client, `/empresas/${idBusiness}/comercios/${values.id}`), {
+
+            abierto: e.target.checked,
+
+        });
+
+        (e.target.checked)
+            ? toast.success("Atendiendo")
+            : toast.success("Cerrado")
+
+
+        setLoading2(false);
+    };
 
     useEffect(() => {
         getDoc(values.sectorEconomico)
@@ -25,11 +56,16 @@ export const RowStores = ({ values }: { values: Store }) => {
             });
     }, []);
 
-    const { idBusiness } = router.query;
-
     return (
         <>
             <tr>
+                <td>
+                    <div className="form-control">
+                        <label className="cursor-pointer label">
+                            <input type="checkbox"  className="checkbox checkbox-secondary" />
+                        </label>
+                    </div>
+                </td>
                 <td>
                     <img
                         src={values.banner}
@@ -40,17 +76,49 @@ export const RowStores = ({ values }: { values: Store }) => {
                 <td>{!loading && nameSector}</td>
                 <td> {values.nombre} </td>
                 <td>
-                    {(values.abierto)
-                        ? 'Abierto'
-                        : 'Cerrado'}
-                </td>
-                <td className='flex'>
-                    <div className="form-control mr-3">
-                        <label className="label cursor-pointer">
-                            <input type="checkbox" className="toggle toggle-accent" checked />
-                        </label>
-                    </div>
+                    {
+                        (loading)
+                            ?
+                            (
+                                <div
+                                    className='spinner-border text-light spinner-border-sm'
+                                    role='status'>
+                                    <span className='visually-hidden'>Loading...</span>
+                                </div>
 
+                            )
+                            : (
+                                < input
+                                    type="checkbox"
+                                    className="toggle toggle-secondary h-[1.2rem]"
+                                    checked={values.status}
+                                    onChange={handleStatusChange}
+                                />
+                            )
+                    }
+                </td>
+                <td>
+                    <label className="label cursor-pointer">
+                        {
+                            (loading2)
+                                ?
+                                (<p>loading...</p>)
+                                :
+                                (
+                                    <input
+                                        type="checkbox"
+                                        className="toggle toggle-secondary h-[1.2rem]"
+                                        checked={values.abierto}
+                                        onChange={handleStatus}
+                                    />
+                                )
+                        }
+
+                    </label>
+
+                </td>
+
+                <td className='flex'>
                     <Link href={`/empresas/${idBusiness}/tiendas/${values.id}/productos/`}>
                         <button className='btn btn-primary btn-sm mr-3 mt-1' >
                             Productos
--- src/pages/empresas/[idBusiness]/tiendas/index.tsx
+++ src/pages/empresas/[idBusiness]/tiendas/index.tsx
@@ -1,24 +1,61 @@
 import { collection, query, orderBy } from "firebase/firestore";
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
+import { useState } from "react";
 import { Dashboard } from "../../../../layout/Dashboard/Dashboard";
 import { db_client } from "../../../../firebase/client";
 import { RowStores } from "../../../../app/business/stores/RowStores";
 import { FirebaseDataTable } from "../../../../components/FirebaseDataTable/FirebaseDataTable";
+import { Commerce } from "../../../../types/comerce";
+import { ChangeEvent } from "react";
 const Business = () => {
+    const [commerce, setCommerce] = useState<Commerce[]>([]);
+    const [selectedStores, setSelectedStores] = useState<string[]>([]);
+
     const router = useRouter();
     const { idBusiness } = router.query;
+
+    const handleSelectAll = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.checked;
+    
+        if (value) {
+          if (commerce.length) {
+            setSelectedStores(commerce.map((com) => com.id));
+          }
+        } else {
+          setSelectedStores([]);
+        }
+      };
+      
     return (
         <Dashboard>
+            <div className="flex">
+                <button className='btn btn-primary btn-sm mr-3 mt-1 mb-2' >
+                    Verficar Seleccionados
+                </button>
+                <div className="form-control flex justify-end">
+                    <label className="cursor-pointer label">
+                        <input
+                            type="checkbox"
+                            className="checkbox checkbox-secondary mr-3"
+                            onChange={handleSelectAll}
+                        />
+                        <span className="label-text">Seleccionar Todos</span>
+                    </label>
+                </div>
+            </div>
+          
             <FirebaseDataTable
                 headers={[
+                    ` `,
                     'Imagen',
                     'Sector Economico',
                     'Nombre',
-                    'Atencion',
+                    'Verificación',
+                    'Atención',
                     'ACCIONES',
                 ]}
                 RowComponent={RowStores}
-                qi={query(collection(db_client, `empresas/${idBusiness}/comercios`),orderBy('id'))}
+                qi={query(collection(db_client, `empresas/${idBusiness}/comercios`), orderBy('id'))}
             />
         </Dashboard>
     );
