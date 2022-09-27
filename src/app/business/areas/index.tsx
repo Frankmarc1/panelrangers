1a859441d1e86410cace8c4c34e3ea9c5daede11
Termux-Master93 <brisantur8034@gmail.com>
2022-09-27 08:17:59 -0500

    llamar listado de areas

---
+++ src/app/business/areas/index.tsx
@@ -0,0 +1,10 @@
+import { Areas } from './Areas'
+import { MapProvider } from './componets/stateManagement/MapProvider'
+
+export const Index = (): JSX.Element => {
+    return (
+        <MapProvider>
+            <Areas />
+        </MapProvider>
+    )
+}
